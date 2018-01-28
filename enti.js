ecf6b39f61ff49049e693eba70e636d6a833d4cd
Tameryesildag <tameryesildag321@gmail.com>
2018-01-28 19:25:03 +0300

    Add files via upload

--- enti.js
+++ enti.js
@@ -1,8 +1,8 @@
 function enti(name,width,height,simage) {
     this.x = random(100, document.body.clientWidth - 50);
     this.y = 10;                    //random(100, 500)
-    this.width = width / 7;
-    this.height = height / 7;
+    this.width = width;
+    this.height = height;
     this.name = name;
     this.simage = simage;
   }
